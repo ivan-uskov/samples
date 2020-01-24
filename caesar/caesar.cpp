709b45cbbcf7d5fbe7628adb4352817c78ed5333
Ivan Uskov <ivan.uskov@ispringsolutions.com>
2020-01-24 12:42:36 +0300

    Реализовал алгоритм

--- caesar/caesar.cpp
+++ caesar/caesar.cpp
@@ -1,43 +1,59 @@
 #include <iostream>
 #include <functional>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
-void crypt(istream & in, ostream & out)
+using Type = uint8_t;
+
+template <typename T = uint8_t>
+void crypt(istream & in, ostream & out, unsigned key)
+{
+    transform(std::istream_iterator<T>(in), std::istream_iterator<T>(), std::ostream_iterator<T>(out), [key](T v) {
+        return T((v + key) % std::numeric_limits<T>::max());
+    });
+}
+
+template <typename T = uint8_t>
+void decrypt(istream & in, ostream & out, unsigned key)
+{
+    transform(std::istream_iterator<T>(in), std::istream_iterator<T>(), std::ostream_iterator<T>(out), [key](T v) {
+        return T((v - key) % std::numeric_limits<T>::max());
+    });
+}
+
+void analyze(istream & in, ostream & out)
 {
     std::string str((std::istreambuf_iterator<char>(cin)),std::istreambuf_iterator<char>());
 }
 
-void decrypt(istream & in, ostream & out)
-{
-
-}
-
 void help(ostream & out)
 {
-
+    out << "Caesar Implementation Useage:" << endl;
+    out << "~$ caesar crypt <key>" << endl;
+    out << "~$ caesar decrypt <key>" << endl;
+    out << "~$ caesar analyze" << endl;
+    out << "~$ caesar help" << endl;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc > 1)
+    if (argc > 2 && argv[1] == string("decrypt"))
     {
-        if (argv[1] == "decrypt")
-        {
-            decrypt(cin, cout);
-        }
-        else if (argv[1] != "crypt")
-        {
-            crypt(cin, cout);
-        }
-        else
-        {
-            help(cout);
-        }
+        decrypt(cin, cout, stoul(argv[2]));
+    }
+    else if (argc > 2 && argv[1] == string("crypt"))
+    {
+        crypt(cin, cout, stoul(argv[2]));
+    }
+    else if (argc > 1 && argv[1] == string("analyze"))
+    {
+        analyze(cin, cout);
     }
     else
     {
-        crypt(cin, cout);
+        help(cout);
     }
 
     return 0;
