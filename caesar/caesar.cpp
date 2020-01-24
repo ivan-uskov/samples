a9b306ac1e7f8c0517705a59ef9c6eb84c628506
Ivan Uskov <ivan.uskov@ispringsolutions.com>
2020-01-24 11:03:24 +0300

    add caesar impl

--- caesar/caesar.cpp
+++ caesar/caesar.cpp
@@ -1,3 +1,44 @@
 #include <iostream>
+#include <functional>
 
+using namespace std;
 
+void crypt(istream & in, ostream & out)
+{
+    std::string str((std::istreambuf_iterator<char>(cin)),std::istreambuf_iterator<char>());
+}
+
+void decrypt(istream & in, ostream & out)
+{
+
+}
+
+void help(ostream & out)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1)
+    {
+        if (argv[1] == "decrypt")
+        {
+            decrypt(cin, cout);
+        }
+        else if (argv[1] != "crypt")
+        {
+            crypt(cin, cout);
+        }
+        else
+        {
+            help(cout);
+        }
+    }
+    else
+    {
+        crypt(cin, cout);
+    }
+
+    return 0;
+}
